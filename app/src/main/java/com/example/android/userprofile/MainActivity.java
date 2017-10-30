1f0e061226f4b486a17619d205dd492f07517005
jlin27 <jessica.lin@udacity.com>
2017-10-30 16:58:22 -0700

    1.02-Solution-CreateTextView

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -12,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText("Vicky");
+
+        TextView birthday = (TextView) findViewById(R.id.birthday);
+        birthday.setText("9/24/1996");
+
+        TextView school = (TextView) findViewById(R.id.school);
+        school.setText("UC Berkeley");
     }
 }
