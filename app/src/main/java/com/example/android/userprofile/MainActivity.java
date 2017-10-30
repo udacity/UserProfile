cf15681b24dff84535821f52b6345adea6cf108f
jlin27 <jessica.lin@udacity.com>
2017-10-30 16:43:43 -0700

    1.02-Solution-CreateTextView

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.userprofile;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText("Vicky");
+        /** Set your profile name size*/
+        name.setTextSize(30);
+        /** Set your profile name color*/
+        name.setTextColor(Color.BLUE);
+
+        TextView birthday = (TextView) findViewById(R.id.birthday);
+        birthday.setText("9/24/1996");
+
+        TextView school = (TextView) findViewById(R.id.school);
+        school.setText("UC Berkeley");
     }
 }
