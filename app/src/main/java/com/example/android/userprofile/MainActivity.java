f682707c8fa06cf20eb932e396ac4def4ccf3591
jlin27 <jessica.lin@udacity.com>
2017-10-30 16:55:31 -0700

    1.01-Solution-Layout

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -12,21 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ImageView picture = (ImageView) findViewById(R.id.profile_picture);
-        picture.setImageResource(R.drawable.picture);
-
-        TextView name = (TextView) findViewById(R.id.name);
-        name.setText("Vicky");
-        /** Set your profile name size*/
-        name.setTextSize(30);
-        /** Set your profile name color*/
-        name.setTextColor(Color.BLUE);
-
-        TextView birthday = (TextView) findViewById(R.id.birthday);
-        birthday.setText("9/24/1996");
-
-        TextView school = (TextView) findViewById(R.id.school);
-        school.setText("UC Berkeley");
     }
 }
