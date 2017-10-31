d7c2d2b7c174313f94665d6c7a900dd6c90879ce
jlin27 <jessica.lin@udacity.com>
2017-10-30 17:13:21 -0700

    1.03-Solution-AddImageView

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -19,11 +19,6 @@ public class MainActivity extends AppCompatActivity {
         TextView name = (TextView) findViewById(R.id.name);
         name.setText("Vicky");
 
-        /** Set your profile name size*/
-        name.setTextSize(30);
-        /** Set your profile name color*/
-        name.setTextColor(Color.BLUE);
-
         TextView birthday = (TextView) findViewById(R.id.birthday);
         birthday.setText("9/24/1996");
 
